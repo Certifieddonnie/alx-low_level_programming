973d8450c114a022ee13c5065c04bf5cfe509b47
Mcdonald <mcdonaldsamure91@gmail.com>
2022-07-07 11:12:23 -0700

    Fixed it

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,6 +2,10 @@
 
 /**
  * more_numbers - Function to print 0 to 14 ten times
+ * i: the number of time the numbers are printed
+ * j: the first value of the two digits numbers
+ * k: the second value of the two digits numbers
+ * m: the one digit numbers represented by k
  *
  * Return: nothing
  */
@@ -10,14 +14,25 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int k;
+	int m;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (m = 0; m <= 14; m++)
 		{
-			_putchar((j) + '0');
+			if (m < 10)
+			{
+				k = m;
+			}
+			else
+			{
+				j = m / 10;
+				k = m % 10;
+				_putchar(j + '0');
+			}
+			_putchar(k + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }

5c596bfc7ce2dcaca28387d73af9815d9b0f6d39
Mcdonald <mcdonaldsamure91@gmail.com>
2022-07-07 15:29:37 -0700

    Advanced

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,13 +14,13 @@ void print_number(int n)
 		if (n < 0)
 		{
 			_putchar('-');
-			n = n * -1;
+			n = -n;
 		}
 
 		if (n / 10)
 			print_number(n / 10);
 
-		_putchar((n % 10) + '0');
+		_putchar(n % 10 + '0');
 	}
 	else
 		_putchar('0');

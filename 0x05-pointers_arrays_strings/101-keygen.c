cd9785f4df9f00c1a6dc8621fa681aec72e85406
Mcdonald <mcdonaldsamure91@gmail.com>
2022-07-13 03:35:16 -0700

    Dont hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -16,44 +16,20 @@ int main(void)
 	int i = 0;
 
 	int rands = 0;
+	int total = 0;
 
 	srand((unsigned int)(time(NULL)));
 
-	char numbers[] = "0123456789";
-
-	char letters[] = "abcdefghijklmnopqrstuvwxyz";
-
-	char symbols[] = "!@#$^&?*";
-
-	char LETTERS[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 	char password[100];
 
-	rands = rand() % 4;
-
-	for (i = 0; i <= 10; i++)
+	for (i = 0; total <= 2644; i++)
 	{
-		if (rands == 1)
-		{
-			password[i] = numbers[rand() % 10];
-			rands = rand() % 4;
-		}
-		else if (rands == 2)
-		{
-			password[i] = symbols[rand() % 8];
-			rands = rand() % 4;
-		}
-		else if (rands == 3)
-		{
-			password[i] = letters[rand() % 26];
-			rands = rand() % 4;
-		}
-		else
-		{
-			password[i] = LETTERS[rand() % 26];
-			rands = rand() % 4;
-		}
+		rands = (rand() % 25) + 65;
+		password[i] = rands;
+		total = total + rands;
 	}
+
+	password[i++] = 2772 - total;
 	password[i++] = '\0';
 	printf("%s\n", password);
 

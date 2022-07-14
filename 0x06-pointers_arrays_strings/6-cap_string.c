e7c2bc0c1d109014a8224665854afb76853b4e0d
Mcdonald <mcdonaldsamure91@gmail.com>
2022-07-14 14:34:44 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,11 +9,16 @@
 
 char *cap_string(char *s)
 {
-	int i;
+	int i = 0;
 	int sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 	int j;
+	if (s[i] >= 'a' && s[i] <= 'z')
+	{
+		s[i] = s[i] - 32;
+	}
+	i++;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 13; j++)
 		{
@@ -23,6 +28,7 @@ char *cap_string(char *s)
 				{
 					s[i + 1] = s[i + 1] - 32;
 				}
+				break;
 			}
 		}
 	}

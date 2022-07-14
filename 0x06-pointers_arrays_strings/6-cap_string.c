278fb790500f1d2ec6b4e81774c978343e5a00f1
Mcdonald <mcdonaldsamure91@gmail.com>
2022-07-14 14:24:52 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * cap_string - Function to capitalize the first letter of every word.
+ * @s: string
+ *
+ * Return: always the result s.
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+	int sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] = s[i + 1] - 32;
+				}
+			}
+		}
+	}
+
+	return (s);
+}

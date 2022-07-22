8da98abcf2c76718370f6a2d114d8105b33ea9a3
Mcdonald <mcdonaldsamure91@gmail.com>
2022-07-22 07:31:12 -0700

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,15 +14,12 @@ int main(int argc, char *argv[])
 {
 	int mul;
 
-	if (argc > 1 && argc < 4)
-	{
-		mul = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", mul);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
 	return (0);
 }

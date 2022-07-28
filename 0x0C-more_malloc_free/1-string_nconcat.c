8c2e0d2edafccafc60aec1b8d1d257689ed53cc1
Mcdonald <mcdonaldsamure91@gmail.com>
2022-07-28 06:24:57 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,26 +36,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	p = _strlen(s1), q = _strlen(s2);
-	if (n > (q - 1))
-	{
-		size = p + (q + 1);
-		cat = malloc(size * sizeof(char *));
-		if (cat == 0)
-			return (NULL);
-		for (i = 0; s1[i] != '\0'; i++)
-			cat[i] = s1[i];
-		for (j = 0; s2[j] != '\0'; j++)
-		{
-			cat[i] = s2[j];
-			i++;
-		}
-		cat[i] = '\0';
-		return (cat);
-	}
-	size = p + (n + 1);
+	p = _strlen(s1);
+	q = _strlen(s2);
+	if (n > q)
+		n = q;
+	size = p + n + 1;
 	cat = malloc(size * sizeof(char *));
-	if (cat == 0)
+	if (cat == NULL)
 	{
 		free(cat);
 		return (NULL);

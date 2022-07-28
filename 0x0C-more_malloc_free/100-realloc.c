ba97c11e091c0f589af15937afbb0e9ec4a78483
Mcdonald <mcdonaldsamure91@gmail.com>
2022-07-28 08:15:24 -0700

    Reallocation

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if (!ptr)
+	if (ptr == NULL)
 		ptr = malloc(new_size);
 	if (new_size == old_size)
 		return (ptr);

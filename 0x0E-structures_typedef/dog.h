47f066cf734544fd585116a5bd9b18d8f09d5441
Mcdonald <mcdonaldsamure91@gmail.com>
2022-08-01 12:57:50 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - my dog info
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: dog owner
+ *
+ * Description: Gives the information about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

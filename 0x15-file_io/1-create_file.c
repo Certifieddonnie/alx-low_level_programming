7c88473cebe2a8b1222dbe7baff12b8b46ace2ad
Mcdonald <mcdonaldsamure91@gmail.com>
2022-08-22 09:08:05 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,10 +11,10 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, i, count;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 

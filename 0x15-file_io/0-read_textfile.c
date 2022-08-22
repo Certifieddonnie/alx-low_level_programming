d6475ee4866009adf7bb5dd8dd6467f293e72ed5
Mcdonald <mcdonaldsamure91@gmail.com>
2022-08-22 07:47:29 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,10 +14,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t count, read_count;
 	char *buf;
 
-	if (*filename == NULL)
+	if (filename == NULL)
 		return (0);
 
-	fd = open(*filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);

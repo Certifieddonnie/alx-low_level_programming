f408a04faced99ef68a7bf1f037179a42db5115e
Certifieddonnie <mcdonaldsamure91@gmail.com>
2022-10-24 11:12:22 +0100

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,29 +7,28 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *new_hash;
-  unsigned long int i;
+	hash_table_t *new_hash;
+	unsigned long int i;
 
-  new_hash = malloc(sizeof(hash_table_t));
-  if (!new_hash)
-  {
-    return(NULL);
-  }
+	new_hash = malloc(sizeof(hash_table_t));
+	if (!new_hash)
+	{
+		return (NULL);
+	}
 
-  new_hash->size = size;
-  new_hash->array = malloc(sizeof(hash_node_t *) * size);
-  if (!new_hash->array)
-  {
-    return(NULL);
-  }
+	new_hash->size = size;
+	new_hash->array = malloc(sizeof(hash_node_t *) * size);
+	if (!new_hash->array)
+	{
+		return (NULL);
+	}
 
-  i = 0;
-  while(i < size)
-  {
-    new_hash->array[i] = NULL;
-    i++;
-  }
+	i = 0;
+	while (i < size)
+	{
+		new_hash->array[i] = NULL;
+		i++;
+	}
 
-  return(new_hash);
-
+	return (new_hash);
 }
